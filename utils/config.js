3fc83984c929d33a6f7c57f0343ced48a9bad710
AKoivisto <antton.v.koivisto@student.jyu.fi>
2024-11-26 18:37:14 +0200

    4.2

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 const password = process.env.SALIS
+const PORT=process.env.PORT
 const MONGODB_URI = `mongodb+srv://anttonvkoivisto:${password}@phonebook-backend.608sw.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Phonebook-backend`
 
 module.exports = {
-    MONGODB_URI
+    MONGODB_URI,
+    PORT
 }

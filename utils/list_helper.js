e3f442d1b253b64cc2a0fc4c48816bebf3907d1b
AKoivisto <antton.v.koivisto@student.jyu.fi>
2024-11-29 18:00:15 +0200

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1    
 }
@@ -6,7 +8,30 @@ const totalLikes = (blogs) => {
     return blogs.reduce((summa, blog) => summa+blog.likes,0)
     }
 
+const favoriteBlog = (blogs) => {
+    mostLikes = blogs.reduce((max, blog) =>
+        blog.likes > max.likes ? blog : max)
+    const toReturn = {
+        title: mostLikes.title,
+        author: mostLikes.author,
+        likes: mostLikes.likes,
+    }
+    return toReturn
+}
+
+const mostBlogs = (blogs) => {
+    const topWriter = _(blogs)
+    .groupBy('author')
+    .map((blogs,author) => ({author, count: blogs.length}))
+    .maxBy('count')
+    console.log(topWriter)
+    return topWriter
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
+

a6c18b574708cf46b6dbee57118f29c7f5d00ea7
AKoivisto <antton.v.koivisto@student.jyu.fi>
2025-01-06 10:45:25 +0200

    user.js and users.js

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt')
+const { request, response } = require('../app')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const User = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
+
+

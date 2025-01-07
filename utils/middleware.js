15f1142865af32b55d2e925194043a384e2501d9
AKoivisto <antton.v.koivisto@student.jyu.fi>
2025-01-07 14:27:21 +0200

    4.19

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,7 +5,10 @@ const errorHandler = (error, request, response, next) => {
       return response.status(400).json({ error: error.message })
     } else if (error.message.includes('E11000')) {
       return response.status(400).json({ error: 'expected `username` to be unique' })
+    } else if (error.name ===  'JsonWebTokenError') {
+      return response.status(400).json({ error: 'token missing or invalid' })
     }
+  
   
     next(error)
   }
